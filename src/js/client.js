ce09e9e04274b0be160149883a9a173bfb4da19c
akine <weezer.and.you@gmail.com>
2020-05-20 16:57:24 +0900

    add Layout component

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-class Layout extends React.Component {
-  render() {
-    return (
-      <h1>lets go lets go!</h1>
-    );
-  }
-}
+import Layout from "./components/Layout"
 
 const app = document.getElementById('app');
 ReactDom.render(<Layout />, app)
